7f49a7b2b73e57bea7c948776833171756b3ca9d
cheny39 <54039797+cheny39@users.noreply.github.com>
2024-09-22 10:32:56 -0500

    upload files

---
+++ script/stimulus.js
@@ -0,0 +1,28 @@
+const VIDEO_ELEMENT_ID = "videoElement";
+const WRONG_BUTTON_ELEMENT_ID = "wrongButton";
+const CORRECT_BUTTON_ELEMENT_ID = "correctButton";
+
+/**
+ * Generates a exposure stimulus HTML element.
+ * 
+ * @param {string} video - The path to the video file.
+ * @param {string} wrong_button_image - The path to the wrong button image file.
+ * @param {string} wrong_button_position - The position of the wrong button.
+ * @returns {string} The HTML string for the video stimulus.
+ */
+function generateExposureStimulus(video, wrong_button_image, wrong_button_position) {
+    return `<div>
+                <video id="${VIDEO_ELEMENT_ID}"><source src="${video}" type="video/mp4">Your browser does not support the video tag.</video>
+                <button id="${WRONG_BUTTON_ELEMENT_ID}" class="jspsych-btn" style="position:absolute; left:${wrong_button_position.left}px; top: ${wrong_button_position.top}px">
+                        <img src="${wrong_button_image}" style="width:240px; height:135px;">
+                </button>
+            </div>`;
+}
+
+function generateAttentionCheckStimulus(wrong_button_image, wrong_button_position) {
+    return `<div>
+                <button id="${WRONG_BUTTON_ELEMENT_ID}" class="jspsych-btn" style="position:absolute; left:${wrong_button_position.left}px; top: ${wrong_button_position.top}px">
+                        <img src="${wrong_button_image}" style="width:240px; height:135px;">
+                </button>
+            </div>`;
+}
