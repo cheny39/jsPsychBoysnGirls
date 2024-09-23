8437a2f83aa1c0a92c108209dd0ec3c584b98e51
Huiji Ying <ying.hj1995@gmail.com>
2024-09-22 23:15:55 -0400

    Remove unneeded trial_duration in instruction trials.

--- script/instruction_trial.js
+++ script/instruction_trial.js
@@ -9,13 +9,7 @@ let instruction_trial = {
         task: "instruction",
         image: jsPsych.timelineVariable('image'),
         audio: jsPsych.timelineVariable('audio'),
-        trial_duration: jsPsych.timelineVariable('trial_duration')
     },
+    // Need full path to the audio file here (e.g. including the wav/ prefix)
     audio: jsPsych.timelineVariable('audio'),
-    // trial_duration: jsPsych.timelineVariable('trial_duration'),
-    // on_load: async function () {
-    //     // Play audio and show buttons at the same time
-    //     const audio = jsPsych.evaluateTimelineVariable('audio');
-    //     playSound(`wav/${audio}`);
-    // }
 };
--- script/procedures/instruction.js
+++ script/procedures/instruction.js
@@ -1,43 +1,37 @@
 let instruction_trial_procedure = {
     timeline: [instruction_trial],
     timeline_variables:
-        [{ 'audio': 'wav/1.Hi_I_am_a_Goolu.wav', 'image': 'intro_gulu1.png', 'trial_duration': 4000 },
-        { 'audio': 'wav/2.I_am_a_Gooloo.wav', 'image': 'intro_gulu6.png', 'trial_duration': 4000 },
-        { 'audio': 'wav/3.I_am_also_a_Gooloo.wav', 'image': 'intro_gulu7.png', 'trial_duration': 4000 },
-        { 'audio': 'wav/v2_4.We_are_gulus_combined.wav', 'image': 'intro_gulus_v2.png', 'trial_duration': 4000 },
-        { 'audio': 'wav/7.Hi_I_am_a_Norl.wav', 'image': 'intro_norl1.png', 'trial_duration': 4000 },
-        { 'audio': 'wav/v2_8.I_am_a_Norl.wav', 'image': 'intro_norl6.png', 'trial_duration': 4000 },
-        { 'audio': 'wav/v2_9.I_am_a_Norl_too.wav', 'image': 'intro_norl5.png', 'trial_duration': 4000 },
-        { 'audio': 'wav/v2_10.we_are_norls_combined.wav', 'image': 'intro_norls_v2.png', 'trial_duration': 4000 },
+        [{ 'audio': 'wav/1.Hi_I_am_a_Goolu.wav', 'image': 'intro_gulu1.png' },
+        { 'audio': 'wav/2.I_am_a_Gooloo.wav', 'image': 'intro_gulu6.png' },
+        { 'audio': 'wav/3.I_am_also_a_Gooloo.wav', 'image': 'intro_gulu7.png' },
+        { 'audio': 'wav/v2_4.We_are_gulus_combined.wav', 'image': 'intro_gulus_v2.png' },
+        { 'audio': 'wav/7.Hi_I_am_a_Norl.wav', 'image': 'intro_norl1.png' },
+        { 'audio': 'wav/v2_8.I_am_a_Norl.wav', 'image': 'intro_norl6.png' },
+        { 'audio': 'wav/v2_9.I_am_a_Norl_too.wav', 'image': 'intro_norl5.png' },
+        { 'audio': 'wav/v2_10.we_are_norls_combined.wav', 'image': 'intro_norls_v2.png' },
         {
             'audio': 'wav/v2_13.Gooloos_and_Norls_come_from_di.wav',
-            'image': 'intro_gulus_speaking.png',
-            'trial_duration': 4000
+            'image': 'intro_gulus_speaking.png'
         },
-        { 'audio': 'wav/14.And_Norls_are_red.wav', 'image': 'intro_norls_speaking.png', 'trial_duration': 4000 },
+        { 'audio': 'wav/14.And_Norls_are_red.wav', 'image': 'intro_norls_speaking.png' },
         {
             'audio': 'wav/15.Gooloos_and_norls_like_differe.wav',
-            'image': 'intro_gulus_speaking.png',
-            'trial_duration': 4000
+            'image': 'intro_gulus_speaking.png'
         },
         {
             'audio': 'wav/16.And_norls_really_like_cheese.wav',
-            'image': 'intro_norls_speaking.png',
-            'trial_duration': 4000
+            'image': 'intro_norls_speaking.png'
         },
         {
             'audio': 'wav/v2_15.We_took_some_pictures_of_the_a.wav',
-            'image': 'intro_gulus_speaking.png',
-            'trial_duration': 4000
+            'image': 'intro_gulus_speaking.png'
         },
         {
             'audio': 'wav/v2_16.Gooloos_and_Norls_speak_slight.wav',
-            'image': 'intro_norls_speaking.png',
-            'trial_duration': 4000
+            'image': 'intro_norls_speaking.png'
         },
         {
             'audio': 'wav/v2_25.lets_practice_touching.wav',
-            'image': 'intro_gulus_speaking.png',
-            'trial_duration': 4000
+            'image': 'intro_gulus_speaking.png'
         }]
 };
