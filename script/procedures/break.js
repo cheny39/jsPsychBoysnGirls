92d49372d64cc1651a1c2ec691a367e1efc7ee2a
Yiran Chen <cheny39@sas.upenn.edu>
2024-09-25 14:41:02 -0400

    comment out beginning for troubleshooting

--- script/procedures/break.js
+++ script/procedures/break.js
@@ -6,13 +6,13 @@ let break_trial_procedure1 = {
     ...break_trial_procedure_base,
     timeline_variables: [
         {
-            audio: "found_more_animals.wav",
-            image: "break1_new.png",
+            'audio': "wav/found_more_animals.wav",
+            'image': "break1_new.png",
             // trial_duration: 4000,
-            image_position: {
-                left: 0,
-                top: 0
-            }
+            // image_position: {
+            //     left: 0,
+            //     top: 0
+            // }
         },
     ]
 };
@@ -20,13 +20,13 @@ let break_trial_procedure2 = {
     ...break_trial_procedure_base,
     timeline_variables: [
         {
-            audio: "found_more_animals.wav",
-            image: "break2_new.png",
-            // trial_duration: 4000,
-            image_position: {
-                left: 0,
-                top: 0
-            }
+            'audio': "wav/found_more_animals.wav",
+            'image': "break2_new.png",
+            // // trial_duration: 4000,
+            // image_position: {
+            //     left: 0,
+            //     top: 0
+            // }
         },
     ]
 };
@@ -34,13 +34,13 @@ let break_trial_procedure3 = {
     ...break_trial_procedure_base,
     timeline_variables: [
         {
-            audio: "found_more_animals.wav",
-            image: "break3_new.png",
-            // trial_duration: 4000,
-            image_position: {
-                left: 0,
-                top: 0
-            }
+            'audio': "wav/found_more_animals.wav",
+            'image': "break3_new.png",
+            // // trial_duration: 4000,
+            // image_position: {
+            //     left: 0,
+            //     top: 0
+            // }
         },
     ]
 };
@@ -48,13 +48,13 @@ let break_trial_procedure4 = {
     ...break_trial_procedure_base,
     timeline_variables: [
         {
-            audio: "found_more_animals.wav",
-            image: "break4_new.png",
-            // trial_duration: 4000,
-            image_position: {
-                left: 0,
-                top: 0
-            }
+            'audio': "wav/found_more_animals.wav",
+            'image': "break4_new.png",
+            // // trial_duration: 4000,
+            // image_position: {
+            //     left: 0,
+            //     top: 0
+            // }
         },
     ]
 };
